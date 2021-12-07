58ed510b1f885399e1de498f50acc9af70d9c880
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-12-07 11:53:58 +0530

    changed runner name

--- src/test/java/com/browserstack/tests/TestRunner.java
+++ src/test/java/com/browserstack/tests/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/",
+@CucumberOptions(features = "src/test/resources/features/demo2.feature",
         glue = {""})
-public class DemoTest1 {
+public class TestRunner {
 }
